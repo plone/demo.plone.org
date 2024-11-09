16401cca0c316a8e128fc5c52377c173df82581e
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-11-09 09:52:24 +0100

    Update backend to Plone 6.1b1

---
+++ backend/tests/conftest.py
@@ -0,0 +1,18 @@
+from plone6.demo.testing import ACCEPTANCE_TESTING
+from plone6.demo.testing import FUNCTIONAL_TESTING
+from plone6.demo.testing import INTEGRATION_TESTING
+from pytest_plone import fixtures_factory
+
+
+pytest_plugins = ["pytest_plone"]
+
+
+globals().update(
+    fixtures_factory(
+        (
+            (ACCEPTANCE_TESTING, "acceptance"),
+            (FUNCTIONAL_TESTING, "functional"),
+            (INTEGRATION_TESTING, "integration"),
+        )
+    )
+)
