109f1c0247186b8e379e6858b58d909f218ea48d
David Glick <david@glicksoftware.com>
2026-03-05 17:33:06 -0800

    Copy system block to frontend-next

---
+++ frontend-next/packages/frontend-next/src/components/index.js
@@ -0,0 +1,15 @@
+/**
+ * Add your components here.
+ * @module components
+ * @example
+ * import Footer from './Footer/Footer';
+ *
+ * export {
+ *   Footer,
+ * };
+ */
+
+import SystemBlockView from './Blocks/System/View';
+import SystemBlockEdit from './Blocks/System/Edit';
+
+export { SystemBlockView, SystemBlockEdit };
