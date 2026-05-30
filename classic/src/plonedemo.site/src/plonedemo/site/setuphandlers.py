537622516e98df530cbfed1cc58c2618ba809972
Philip Bauer <bauer@starzel.de>
2026-05-30 15:04:22 +0200

    handle mosaic tiles in distribution exportimport

--- classic/src/plonedemo.site/src/plonedemo/site/setuphandlers.py
+++ classic/src/plonedemo.site/src/plonedemo/site/setuphandlers.py
@@ -1,4 +1,7 @@
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
+from zope.globalrequest import getRequest
+from zope.interface import alsoProvides
 from zope.interface import implementer
 
 
@@ -8,7 +11,17 @@ class HiddenProfiles:
         return ["plone.volto"]
 
 
+def pre_install(answers):
+    try:
+        from collective.tiles.carousel.interfaces import ICollectiveTilesCarouselLayer
+        request = getRequest()
+        alsoProvides(request, ICollectiveTilesCarouselLayer)
+    except ImportError:
+        pass
+    return answers
+
 def post_install(distribution, site, answers):
     """Post install script for distribution."""
+    api.portal.set_registry_record("plone.enable_link_integrity_checks", True)
     site.setLayout("language-switcher")
     return site
